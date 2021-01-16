6c4062edcfcb93204415e957f47d2ebb1c25f87b
spencersls2000 <73373376+spencersls2000@users.noreply.github.com>
2021-01-15 22:21:43 -0600

    Add files via upload

---
+++ dictionary app/appone.py
@@ -0,0 +1,43 @@
+conn = mysql.connector.connect(
+user = "spencer601_student",
+password = "spencer601_student",
+host = "108.167.140.122",
+database = "spencer601_pm1database"
+)
+conn.close()
+import json
+from difflib import get_close_matches
+
+data = json.load(open("data.json"))
+
+def translate(w):
+    
+    w = w.lower()
+    if w in data:
+        return data[w]
+    elif w.title() in data:
+        return data[w.title()]
+    elif w.isupper() in data:
+        return data[w.isupper()]
+    elif len(get_close_matches(w, data.keys())) > 0:
+        yn = input("Did you mean %s instead? Enter Y if yes, or N if no: "  % get_close_matches(w,data.keys())[0])
+        if yn == "Y":
+            return data[get_close_matches(w,data.keys())[0]]
+        elif yn == "N":
+            return "The word doesn't exist. Please double check it."
+        else:
+            return "We didn't understand your entry."
+    else:
+        return "The word doesn't exist. Please double check it"
+
+word = input("Enter word: ")
+
+output = translate(word)
+
+if type(output) == list:
+    for item in output:
+        print(item)
+
+else:
+    print(output)
+
